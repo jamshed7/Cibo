3964d8303dd050e24d70ab5b867a9297d2c90d36
Adarsh Pai <adarsh9pai@gmail.com>
2018-09-14 10:41:47 -0500

    init commit 🚀

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text> 🌮 Cibo is a local-search engine powered by curated listings and crowd-sourced recommendations to help you find the perfect place to dine around college campuses.</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
