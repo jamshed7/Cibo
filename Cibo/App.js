36192a5d235e7a7423f9b1c8ec48af4b544633b6
Adarsh Pai <adarsh9pai@gmail.com>
2018-11-17 15:47:23 -0600

    Login Screen and Splash done.

--- Cibo/App.js
+++ Cibo/App.js
@@ -1,6 +1,9 @@
 import React from 'react'
-import { StyleSheet, Text, View, Button } from 'react-native'
+import { StyleSheet, Text, View, Image, Dimensions } from 'react-native'
 import * as Expo from 'expo'
+import { Button } from 'react-native-elements'
+import { Ionicons } from '@expo/vector-icons'
+import SvgUri from 'react-native-svg-uri'
 
 export default class App extends React.Component {
   constructor(props){
@@ -46,10 +49,12 @@ export default class App extends React.Component {
 
 const LoginPage = props => {
   return (
-    <View>
-      <Text style={styles.Title}>Cibo</Text>
-      <Text style={styles.header}>Use Cibo with Facebook</Text>
-      <Button title="Sign in with Facebook" onPress={() => props.FacebookLogin()} />
+    <View style = {styles.LoginPage}>
+      <Text style = {styles.Title}>Cibo</Text>
+      <Image width = '5' height = '5' style = {{marginTop: 15,alignSelf:'center'}} source={require('./assets/LoginEating.png')}/>
+      <Text style = {styles.tagline}>Find the best restaurants.{'\n'}Around your campus.{'\n'}Rated by people.</Text>
+      <Text style = {styles.LoginDescription}>Use Cibo with Facebook</Text>
+      <Button title='Login with Facebook' buttonStyle={styles.LoginButton} onPress={() => props.FacebookLogin()}/>
     </View>
   )
 }
@@ -65,16 +70,47 @@ const LoggedInPage = props => {
 
 const styles = StyleSheet.create({
   Title: {
-    fontSize : 75,
-    color: '#4FB0C6'
+    fontSize : 45,
+    color: '#FFFFFF',
+    marginTop: 100,
+    textAlign: 'center',
+    fontWeight: '700'
   },
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#D81159",
     alignItems: "center",
     justifyContent: "center"
   },
-  header: {
-    fontSize: 25
+  LoginDescription: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#FFFFFF',
+    marginTop: 40,
+    alignContent: 'center',
+    textAlign: 'center',
+    fontWeight: '500'
+  },
+  LoginPage: {
+    flex: 1,
+    alignContent:'center',
+    marginTop: 10
+  },
+  tagline:{
+    color:'#FFFFFF',
+    fontSize: 20,
+    alignContent: 'center',
+    textAlign: 'center',
+    marginTop: 100,
+    fontWeight: '300'
+    
+  },
+  LoginButton: {
+    marginTop: 20,
+    borderRadius: 5,
+    width: 200,
+    height: 60,
+    backgroundColor: '#3B5998',
+    alignSelf:'center'
   }
 })
