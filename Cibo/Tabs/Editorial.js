238e29af188b547516f53decef96685b723a8f75
Adarsh Pai <adarsh9pai@gmail.com>
2018-11-21 21:19:16 -0600

    Editorial v1 done.

--- Cibo/Tabs/Editorial.js
+++ Cibo/Tabs/Editorial.js
@@ -2,8 +2,16 @@ import React from 'react'
 import { SafeAreaView,View,ScrollView, Text, StyleSheet, FlatList, Dimensions} from 'react-native'
 import { Header, ListItem, Button } from 'react-native-elements'
 import { Card, CardTitle, CardContent, CardAction, CardButton, CardImage } from 'react-native-material-cards'
+import {createStackNavigator, createAppContainer} from 'react-navigation'
+
+
+
 class Editorial extends React.Component {
   
+  static navigationOptions = {
+    header: null,
+    }
+
   state = {
     data : []
   };
@@ -29,7 +37,7 @@ class Editorial extends React.Component {
         renderItem={({ item }) => (
           <Card style = {{backgroundColor : "#54546c", borderRadius : 10, marginTop:10}}>
             <CardImage source={{uri: item.ImageLink}} title={item.Title} style = {{fontWeight:'200', color:'orange',backgroundColor:'rgba(52, 52, 52, 0.8)'}}/>
-            <CardButton title={Math.ceil(item.Body.length/256) + ' min read by The Cibo Team'} color = "white" />
+            <CardButton onPress={() => this.props.navigation.navigate('Article',{item})} title={Math.ceil(item.Body.length/256) + ' min read by The Cibo Team'} color = "white" />
           </Card>
         )}
       
@@ -40,6 +48,43 @@ class Editorial extends React.Component {
     );
   }
 }
+
+class Article extends React.Component {
+  
+
+  static navigationOptions = {
+    headerStyle: {
+      backgroundColor: '#ffffff',
+    },
+    headerTitleStyle: {
+      color: 'white'
+    }
+    }
+  
+
+  render() {
+    return (
+      <ScrollView style = {{flex:1, backgroundColor:'#ffffff'}}>
+        <Card style = {{backgroundColor : "#ffffff", borderRadius : 10, margin:0,color:'white'}}>
+            <CardImage source={{uri: this.props.navigation.state.params.item.ImageLink}} style = {{fontWeight:'200', color:'orange',backgroundColor:'rgba(52, 52, 52, 0.8)'}}/>
+            <CardTitle  title={this.props.navigation.state.params.item.Title} subtitle = "By the Cibo Team" />
+            <CardContent text={this.props.navigation.state.params.item.Body}/>
+        </Card>
+      </ScrollView>
+    );
+  }
+}
+
+
+
+const AppNavigator = createStackNavigator({
+  Editorial: {
+    screen: Editorial
+  },
+  Article:{
+    screen: Article
+  }
+})
 
 const styles = StyleSheet.create({
   Title: {
@@ -57,4 +102,4 @@ const styles = StyleSheet.create({
     height: (Dimensions.get('window') - 50)/2
   }
 });
-export default Editorial
+export default createAppContainer(AppNavigator)
